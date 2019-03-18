551f79f848fd06f1c92aa28275066733d0a3f037
DavidVanDerGraaf <david@van-der-graaf.net>
2019-03-18 10:32:52 +0100

    H2

---
+++ H2/Divblok.js
@@ -0,0 +1,28 @@
+var bl1 = document.getElementById("bl1");
+bl1.style.backgroundColor = "aqua";
+bl1.style.textAlign = "center";
+
+var bl1text = document.getElementById("bl1text");
+bl1text.style.width = "100px";
+bl1text.style.height = "20px";
+bl1text.style.backgroundColor = "aqua";
+bl1text.style.position = "absolute";
+bl1text.style.top = "20px";
+
+var bl2 = document.getElementById("bl2");
+bl2.style.backgroundColor = "green";
+bl2.style.position = "absolute";
+bl2.style.top = "20px";
+bl2.style.left = "100px";
+bl2.style.textAlign = "center";
+
+var bl3 = document.getElementById("bl3");
+bl3.style.position = "absolute";
+bl3.style.top = "50px";
+bl3.style.left = "200px";
+bl3.style.textAlign = "right";
+
+var bl4 = document.getElementById("bl4");
+bl4.style.backgroundColor = "red";
+bl4.style.position = "absolute";
+bl4.style.left = "300px";
---
+++ H2/Javascript.js
@@ -0,0 +1,4 @@
+document.getElementById("titel").innerHTML = "Dit is de paginatitel";
+document.getElementById("grotekop").innerHTML = "Dit is de subkop";
+document.getElementById("surprised1").src = "img/surprised1.jpg";
+document.getElementById("surprised2").src = "img/surprised2.jpg";
